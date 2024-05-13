55866df7f2b50a384fb0429bd727caa58c112b54
EcclesN <Niall.Eccles@hmhco.com>
2024-05-13 22:43:50 +0100

    Use main nav

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@mantine/core';
 import CreateEvent from './components/create-event/create-event';
 import { useDisclosure } from '@mantine/hooks';
+import MainNav from './components/main-nav/main-nav';
 
 export function App() {
   const [opened, { open, close }] = useDisclosure(false);
   return (
     <div>
+      <MainNav />
       <div className="px-5">
         <Button variant="filled" size="md" fullWidth onClick={() => opened ? close() : open()}>Create Event</Button>
       </div>
