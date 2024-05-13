000a87096593b7c217ad8ad554b38999d3e46ea2
EcclesN <Niall.Eccles@hmhco.com>
2024-05-13 23:08:28 +0100

    Add drawer to main nav

--- src/app/components/main-nav/main-nav.tsx
+++ src/app/components/main-nav/main-nav.tsx
@@ -1,12 +1,20 @@
-import { ActionIcon } from '@mantine/core';
-import { Plus } from 'lucide-react';
+import { ActionIcon, Drawer } from '@mantine/core';
+import { Menu, Plus } from 'lucide-react';
+import { useDisclosure } from '@mantine/hooks';
 
 export function MainNav({ opened, open }: { opened: boolean, open: () => void }) {
+  const [navDrawerOpen, { open: openNavDrawer, close: closeNavDrawer }] = useDisclosure(false);
   return (
     <nav className="flex flex-row justify-between items-center m-3">
-      <div></div>
+      <ActionIcon variant="transparent" aria-label="Open Navigation Drawer" size="xl"
+                  onClick={() => navDrawerOpen ? null : openNavDrawer()}>
+        <Menu />
+      </ActionIcon>
+      <Drawer opened={navDrawerOpen} onClose={closeNavDrawer} title="Authentication">
+        {/* Drawer content */}
+      </Drawer>
       <h1 className="font-bold">futuro</h1>
-      <ActionIcon variant="transparent" aria-label="Settings" size="xl" onClick={() => opened ? null : open()}>
+      <ActionIcon variant="transparent" aria-label="Create new event" size="xl" onClick={() => opened ? null : open()}>
         <Plus />
       </ActionIcon>
     </nav>
